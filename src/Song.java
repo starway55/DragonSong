ef1d024e913ec384c02e461202b4d6fb05eda16d
ZhanpengDiao@Mac <diaozhanpeng1015@gmail.com>
2018-03-16 23:40:52 +1100

    Song

--- src/Song.java
+++ src/Song.java
@@ -1,8 +1,12 @@
 
-abstract class Song {
-    private Type type;
-    private Tune tune;
+public class Song {
+    private int speed;
     private String text;
+//    private Lis paragraphs;
+    
+    public Song(String text) {
+        this.text = text;
+    }
 
-    public abstract void begin();
+    public void begin() {}
 }
