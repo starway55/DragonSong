ed983bb14b2d5dbb78f852a6f4c78528ff127782
ZhanpengDiao@Mac <diaozhanpeng1015@gmail.com>
2018-03-16 22:45:29 +1100

    create entry point

---
+++ src/DragonSong.java
@@ -0,0 +1,13 @@
+
+
+public class DragonSong {
+
+    public void play() {
+
+    }
+
+    public static void main(String[] args) {
+        DragonSong dragonSong = new DragonSong();
+        dragonSong.play();
+    }
+}
