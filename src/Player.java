5b0242317ed89838da089e66fe0253b0ea8ad906
Yun  Zhao <u4709046@anu.edu.au>
2018-03-17 00:48:06 +1100

    WIP-Player

--- src/Player.java
+++ src/Player.java
@@ -5,8 +5,12 @@ import java.util.*;
 public class Player extends Robot{
     private ArrayList<Song> songs = new ArrayList<>();
 
+    private int currentSpeed;
+    private int keyboardDelay = 20;
+
     public Player() throws AWTException {
     }
+
 
     public void addSong(String text) {
         songs.add(new Song(text));
@@ -18,6 +22,7 @@ public class Player extends Robot{
 
     public void play() {
         for(Song s: songs) {
+            this.currentSpeed = s.getSpeed();
             ArrayList<Syllable> ls = s.getNextParagraph();
             ls.forEach(this::pressKey);
         }
@@ -25,5 +30,23 @@ public class Player extends Robot{
 
     private void pressKey(Syllable s) {
 
+        Float msPerSyllable = 60.0f / currentSpeed;
+
+        Float afterKeyPress = msPerSyllable - 2 * keyboardDelay;
+
+        switch(s.getTune()){
+            case L1:
+        }
+
     }
+
+    public void delay(int delay){
+        try {
+            Thread.sleep(delay);
+        } catch (InterruptedException e){
+            e.printStackTrace();
+        }
+    }
+
+
 }
