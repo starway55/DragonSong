2c75996d610ee9ac3902acc274ff4026c3536de8
Yun  Zhao <u4709046@anu.edu.au>
2018-03-16 23:55:59 +1100

    Basic Paragraph Structure

--- src/Song.java
+++ src/Song.java
@@ -1,12 +1,31 @@
+import java.util.List;
 
 public class Song {
     private int speed;
     private String text;
+
+    private List<Paragraph> paragraphs;
 //    private Lis paragraphs;
+
+    public Song(int speed, List<Paragraph> paragraphs){
+        this.speed = speed;
+        this.paragraphs = paragraphs;
+    }
     
     public Song(String text) {
         this.text = text;
     }
 
-    public void begin() {}
+    public void begin() {
+        for(Paragraph p : paragraphs){
+            p.play();
+        }
+    }
+
+    public void playChap (int index){
+        if(index == 0){
+            paragraphs.get(0).play();
+        }
+        paragraphs.get(index-1).play();
+    }
 }
