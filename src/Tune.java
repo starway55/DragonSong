2d49e1c677c248cb8c79ac8f4b374ea27e1e401f
Yun  Zhao <u4709046@anu.edu.au>
2018-03-16 23:12:50 +1100

    Components

--- src/Tune.java
+++ src/Tune.java
@@ -1,2 +1,51 @@
 public enum Tune {
+    /*
+    低音
+    1 #1 2 b3 3 4 #4 5 #5 6 b7 7
+    中音同上
+    高音同上
+    最高音 1
+     */
+
+    L1,
+    LU1,
+    L2,
+    LD3,
+    L3,
+    L4,
+    LU4,
+    L5,
+    LU5,
+    L6,
+    LD7,
+    L7,
+
+    M1,
+    MU1,
+    M2,
+    MD3,
+    M3,
+    M4,
+    MU4,
+    M5,
+    MU5,
+    M6,
+    MD7,
+    M7,
+
+    H1,
+    HU1,
+    H2,
+    HD3,
+    H3,
+    H4,
+    HU4,
+    H5,
+    HU5,
+    H6,
+    HD7,
+    H7,
+
+    HH1,
+    EMPTY
 }
