079a2f0e9aa343ffc642e07b6acfbf49f6eae415
Yun  Zhao <u4709046@anu.edu.au>
2018-03-17 22:29:56 +1100

    WIP - Structure

---
+++ src/TheOriginalSong.java
@@ -0,0 +1,14 @@
+import java.util.List;
+
+public class TheOriginalSong extends Song{
+    private Float speed;
+    private String songString;
+
+    private List<Paragraph> paragraphs;
+
+    public TheOriginalSong(String text, Float speed){
+        super(text, speed);
+    }
+
+
+}
